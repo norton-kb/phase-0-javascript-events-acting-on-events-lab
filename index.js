0c7a37a2b3fbb1e357888fb92dc2887d6dd7db7f
norton-kb <biinorton080@gmail.com>
2024-11-18 23:54:15 +0300

    acting on events

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  // Only move left if dodger is not at the left edge of the game
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  // Only move right if dodger is not at the right edge of the game
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
